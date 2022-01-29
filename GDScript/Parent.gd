80a6f0027cc63b639f92fcc2eb97a9aad5837166
Vlad Govorov <vladoccplaym@gmail.com>
2022-01-29 17:13:29 +1000

    Update Parent.gd

--- GDScript/Parent.gd
+++ GDScript/Parent.gd
@@ -4,13 +4,16 @@ class_name Parent
 export (String) var key
 
 # data previously stored in export variable
-var prop
+var prop: int
 
-var model
+var model: Dictionary
 
 func _ready():
 	# you can use this dictionary as a storage for fields
 	model = Database._data_model[key]
+	# if we want to edit model in-game, we need to duplicate it
+	# otherwise it would change_data_model and all objects created after that
+	model = model.duplicate()
 	
 	# or use predefined function to assign values to vars
 	prop = Database.get_value(key, "prop_key")
